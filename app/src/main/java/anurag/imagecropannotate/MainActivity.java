a2193ca1a7080620bfb5c23817eaf3d122c416f1
Anurag Purwar <anurag.purwar@zigwheels.com>
2018-07-27 14:59:29 +0530

    Initial commit

---
+++ app/src/main/java/anurag/imagecropannotate/MainActivity.java
@@ -0,0 +1,159 @@
+package anurag.imagecropannotate;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+import c.anurag.cropannotate.annotations.activity.PhotoEditorActivity;
+import c.anurag.cropannotate.crop.CropImage;
+import c.anurag.cropannotate.crop.activity.CropImageActivity;
+import c.anurag.cropannotate.crop.utillity.CropImageOptions;
+import c.anurag.cropannotate.util.Utility;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView image;
+    private Context context = this;
+    protected static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE_CAMERA = 0x4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Button gallery = findViewById(R.id.gallery);
+        image = findViewById(R.id.image);
+        gallery.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.gallery:
+                int permissionCheck = PermissionChecker.checkCallingOrSelfPermission(this,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                    openMenu();
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE_CAMERA);
+                }
+                break;
+
+        }
+    }
+
+    private void openMenu() {
+        if (CropImage.isExplicitCameraPermissionRequired(context)) {
+            requestPermissions(
+                    new String[]{Manifest.permission.CAMERA},
+                    CropImage.CAMERA_CAPTURE_PERMISSIONS_REQUEST_CODE);
+        } else {
+            CropImage.startPickImageActivity(this);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case PhotoEditorActivity.REQUEST_CROP_IMAGE:
+                if (resultCode == Activity.RESULT_OK && null != data) {
+                    String path = data.getStringExtra("imagePath");
+                    Uri imageUri = Uri.fromFile(new File(path));
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                        image.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri imageUri = CropImage.getPickImageResultUri(this, data);
+                    Intent intent = new Intent(MainActivity.this, PhotoEditorActivity.class);
+                    intent.putExtra("selectedImagePath", imageUri.toString());
+                    startActivityForResult(intent, PhotoEditorActivity.REQUEST_CROP_IMAGE);
+                    /*Intent intent = new Intent(context, CropImageActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelable(CropImage.CROP_IMAGE_EXTRA_SOURCE, imageUri);
+                    bundle.putParcelable(CropImage.CROP_IMAGE_EXTRA_OPTIONS, mOptions);
+                    intent.putExtra(CropImage.CROP_IMAGE_EXTRA_BUNDLE, bundle);
+                    startActivityForResult(intent, REQUEST_CROP_IMAGE);*/
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                int permissionCheck = PermissionChecker.checkCallingOrSelfPermission(this,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                    openMenu();
+                } else {
+                    Toast.makeText(this, getString(R.string.media_access_denied_msg), Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+}
